2a55963535a161cd365793045ec2c8284a6186b2
Murali M <muralee14@gmail.com>
2012-05-15 11:20:29 +0300

    Change-Id: Ia8b6f6afedc7349476798bb5c1d93dc074ac5460

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -11,12 +11,51 @@ class Bootstrap {
 	// TODO - Insert your code here
 	
 	/**
+	 * @var string
+	 */
+	private $action = '';
+	
+	/**
+	 * @var string
+	 */
+	private $module = 'Index';
+	
+	/**
 	 */
 	function __construct() {
 		
 		// TODO - Insert your code here
 	}
 	
+
+	/**
+	 * @return the $action
+	 */
+	public function getAction() {
+		return $this->action;
+	}
+
+	/**
+	 * @param string $action
+	 */
+	public function setAction($action) {
+		$this->action = $action;
+	}
+
+	/**
+	 * @return the $module
+	 */
+	public function getModule() {
+		return $this->module;
+	}
+
+	/**
+	 * @param string $module
+	 */
+	public function setModule($module) {
+		$this->module = $module;
+	}
+
 	/**
 	 */
 	function __destruct() {
