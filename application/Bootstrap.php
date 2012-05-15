1ceee2cb656572b97616dd521d25285214edd533
Murali M <muralee14@gmail.com>
2012-05-15 11:11:07 +0300

    Change-Id: If77599eaa4e25733ffb5d92c2e7eaa770682caae

---
+++ application/Bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace application;
+
+/**
+ *
+ * @author Murali M
+ *        
+ */
+class Bootstrap {
+	// TODO - Insert your code here
+	
+	/**
+	 */
+	function __construct() {
+		
+		// TODO - Insert your code here
+	}
+	
+	/**
+	 */
+	function __destruct() {
+		
+		// TODO - Insert your code here
+	}
+}
+
+?>
